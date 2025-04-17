fc629bae4890c14232cf794b5a8ed152c9be8a7a
vidy.alfredo1@gmail.com <alfredovidy@gmail.com>
2025-04-17 16:27:08 +0800

    add random number generator

---
+++ src/utilities.ts
@@ -0,0 +1,5 @@
+export function RNG(numDigits = 1) {
+  const min = Math.pow(10, numDigits - 1);
+  const max = Math.pow(10, numDigits) - 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
